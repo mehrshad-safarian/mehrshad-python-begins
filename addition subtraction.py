f5083a75cafddee1496f45997f4d4b1469a12a27
mehrshad-safarian <safar911verydanger@gmail.com>
2025-08-06 23:51:55 +0430

    initial commit

---
+++ addition subtraction.py
@@ -0,0 +1,15 @@
+result0 = 2 + 3 * 4
+print("result0",result0)
+print(f"2 + 3 * 4 = {result0}")
+
+result1 = (2 + 3) * 4
+print("result0",result1)
+print(f"(2 + 3) * 4 = {result1}")
+
+result3 = 10 - 3 ** 4
+print("result3",result0)
+print(f"10 - 3 ** 4 = {result3}")
+
+result4 = (12 - 2) * 5 ** 2
+print("result4",result4)
+print(f"2 - 3 ** 4 = {result4}")
---
+++ operations.py
@@ -0,0 +1,22 @@
+#operations
+addition = 5 + 4
+subtraction = 10 - 4
+multiplication = 4 * 3
+division = 15 / 3
+
+exponent = 2 ** 3
+floor_division = 17 // 5
+modulo = 17 % 5
+
+combined = (4 + 3)
+
+#the execute for output of the code
+print (f""" addition: {addition}
+        subtraction: {subtraction}
+        multiplication: {multiplication} 
+        division: {division}
+        exponent: {exponent}
+        floor_division: {floor_division}
+        modulo: {modulo}
+        combined: {combined}
+""")
