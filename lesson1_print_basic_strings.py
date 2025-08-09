ca362b7282a592a9af4035aa9def885ab735f4f9
mehrshad-safarian <safar911verydanger@gmail.com>
2025-08-10 00:19:10 +0430

    initial commit

---
+++ lesson1_print_basic_strings.py
@@ -0,0 +1,13 @@
+# 1. Print a short story using multiple lines.
+# TODO: print()
+
+
+# 2. Create a string that includes both single and double quotes.
+# TODO:
+
+# 3. Use escape characters to create a formatted list.
+# TODO:
+
+# 4. Combine different types of data (strings and numbers) in one print statement.
+# TODO:
+
