69dfa4f80f901030f8eeb088ea21dc2b18119c94
mehrshad-safarian <safar911verydanger@gmail.com>
2025-08-10 13:21:10 +0430

    new changes

--- lesson1_print_basic_strings.py
+++ lesson1_print_basic_strings.py
@@ -1,13 +1,19 @@
 # 1. Print a short story using multiple lines.
-# TODO: print()
+# TODO:  
+print("PlayStation 2: The Myth")
+print("Back in 1999", "Sony Corporation launched the PlayStation 2,bringing joy and happiness to millions of people around the world.")
+print(" Gamers loved it", "and by the end of its lifetime, it had sold more than 155 million units", "becoming the best-selling console of all time.")
 
 
 # 2. Create a string that includes both single and double quotes.
 # TODO:
-
+print('Memorable quotes from arthur morgan: \n
+"We can't change what's done, we can only move on."\n
+"We are thieves in a world that don’t want us no more."\n
+"Be loyal to what matters."')
 # 3. Use escape characters to create a formatted list.
 # TODO:
-99988
+
 # 4. Combine different types of data (strings and numbers) in one print statement.
 # TODO:
-00088
+
