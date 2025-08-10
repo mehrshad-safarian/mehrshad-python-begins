a7fc7df9f2ddcbd6f14cd42f7c828c243b8ebe6c
mehrshad-safarian <safar911verydanger@gmail.com>
2025-08-10 17:56:09 +0430

    new

---
+++ multi_line_printing.py
@@ -0,0 +1,29 @@
+# Try printing a simple four-line text or poem using three separate print() statements.
+#TODO:
+print("Cho Irân nabâshad")
+print("Tan-e man mabâd")
+print("Bad-in bûm o bar")
+print("Zendeh yek tan mabâd")
+print()
+
+# Try printing a simple four-line text or poem using one print() statement with \n.
+#TODO:
+print("\t Hameh âlam\n\t tan ast,\n\t Irân del\n\t Goyande zin ghis khejel")
+print()
+
+# Print it using triple quotes.
+#TODO:
+print("""Cho Irân nabâshad
+tan-e man mabâd
+Bad-in bûm o bar 
+zendeh yek tan mabâd""")
+print("\n")
+# Add a blanc line between each sting/verse of your text/poem using any method you prefer.
+#TODO:
+print("""Cho Irân nabâshad 
+      
+tan-e man mabâd
+      
+Bad-in bûm o bar 
+      
+zendeh yek tan mabâd""")
