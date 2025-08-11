73fc8eba4dd9fa0621c244462b70cadc4dbde751
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-11 20:21:42 +0000

    changes

--- data-types/using-comments.py
+++ data-types/using-comments.py
@@ -4,3 +4,17 @@ y = 20 # assign value of 20 to variable y
 # calculate the sum of x + y
 result = x + y
 print(result)
+
+#this is a multi-line comment
+#created by using multiple
+#single-line comments
+
+"""
+this multi-line comment 
+crested by using string litrals >>>>double-quotes
+"""
+
+'''
+this multi-line comment 
+crested by using string litrals >>>>single-quotes
+'''
