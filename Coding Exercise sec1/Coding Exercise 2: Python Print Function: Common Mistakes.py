732fad37a01e784cb6e0db0161a575edb871b7ee
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-12 19:03:15 +0000

    new exercise

---
+++ Coding Exercise sec1/Coding Exercise 2: Python Print Function: Common Mistakes.py
@@ -0,0 +1,52 @@
+# Task 1: Print "Hello, World!"
+print("hello, world!")
+# Task 2: Demonstrate the use of both single and double quotes
+# Your code here (use single quotes)
+print('i fight for my goals with all of my try')
+# Your code here (use double quotes)
+print("i love viedo games")
+
+# Task 3: Show correct indentation
+if True:
+    # Your code here (indented)
+# Your code here (not indented)
+      print("indented")
+       #print("not indented")
+
+# Task 4: Demonstrate case sensitivity
+# Your code here
+name = "Midge"
+Name = "Bob"
+print(name) #this gonna print Midge 
+print(Name) #this gonna print Bob
+#but there is also this type of case sensitivity:
+#PRINT(name) #this gonna print Midge 
+#Print(Name) #this gonna print Bob
+#either of these codes gonna work ! 
+
+
+# Task 5: Ensure all parentheses are closed
+# Your code here
+print(" this is closed one which is correct")
+# print("all of sudden i accidentally forgot the closing parenthese so this ain't gonna do the trick XD "  
+print("and now parenthese in parenthes :) ('parentheses')")
+
+# Task 6: Use commas and plus signs in print statements
+# Your code here (using comma)
+# Your code here (using plus sign)
+print("python" + "," + "javascript") #this one put comma between our two words
+print("python" + "javascript") #this one do nothing
+print("python", "javascript") #this one add space between them
+print('python' + ' ' + 'javascript')#this one is also add space between them
+
+# Task 7: Print a backslash character
+# Your code here
+print()
+# Task 8: Mix quotes
+# Your code here
+
+# Task 9: Print multiple items
+# Your code here
+
+# Hint: Remember to use print() function for all tasks
+# Hint: Pay attention to quotation marks and indentation
