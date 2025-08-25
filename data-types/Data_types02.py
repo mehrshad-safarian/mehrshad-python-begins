b0ffd21eebefb86bb685ac914eed92e8ae51dc5e
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-25 17:01:44 +0000

    new codes

---
+++ data-types/Data_types02.py
@@ -0,0 +1,14 @@
+#Uderstanding Data in Python 
+#Common Data Types
+#Numbers
+3, 6, 9 # integers (int)
+3.14, -8.12 # Floats (float)
+# Text
+'this is a string ' # string (str)
+#Boolean
+True, False # Boolean (bool)
+age = 27 # Age is an integer (int)
+pi = 3.14 # pi is float
+name = 'Ronald' # name is a string (str)
+is_new = True # is_new is a Boolean (bool)
+print(type(age)) #check 'age' variable with type() function 
---
+++ data-types/Personal-info.py
@@ -0,0 +1,11 @@
+# Personal Information 
+name = 'mehrshad' # str
+age = 22 # int
+height_meters = 1.77 #float
+is_student = True # bool
+
+# print out the Info
+print('name:', name)
+print('Age:', age)
+print('Height:', height_meters)
+print('Are you a student?', is_student)
