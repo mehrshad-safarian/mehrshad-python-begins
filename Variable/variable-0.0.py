729d3d2a4a4250fc39c1471bf54b3424a958722b
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-26 10:33:38 +0000

    new code

---
+++ Variable/variable-0.0.py
@@ -0,0 +1,17 @@
+#debug the following code 
+'''
+Sport = 'Swim' # capital 's'
+print(sport) # lowercase 's'
+'''
+Sport = 'Swim' 
+print(Sport)
+print()
+
+num1 = 32    # storing an integer into variable
+num2 = 4.56  # storing a float into variable
+print(num1, num2) # we can print multiple item's using comma's 
+print()
+
+#storing boolean (bool) into a vriable 
+switch = False
+print(switch)
---
+++ simple Practice/data_type_id.py
@@ -0,0 +1,18 @@
+''' For each of the following values determine data types:
+42
+3.14
+"Hello, World!"
+True
+-7
+"42"
+False
+0.0
+'''
+print(type(42)) #int
+print(type(3.14)) #float
+print(type("Hello, World!")) #str
+print(type(True)) #Boolean (bool)
+print(type(-7)) #int
+print(type("42")) #str
+print(type(False)) #Boolean (bool)
+print(type(0.0)) #float
