624e16e151749aefedf415fa9eb54ea20bef7f62
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-27 10:35:55 +0000

    new code

---
+++ Variable/variables_solution.py
@@ -0,0 +1,53 @@
+#create a variable named 'age' and assign it to your age as an integer
+age = 27
+"""print(age)"""
+#create a variable called 'height' and assign it to your height in meters as a float
+height = 2.98
+"""print(f"{height} meters")"""
+#create a variable called 'name' and assign it to your name as string 
+name = 'Arthur'
+"""print(name)"""
+# 4. Create a variable called 'is_student' and assign it a boolean value indicating whether you are a student or not.
+is_student = True  
+"""print(is_student)"""
+
+# 5. Print out all these variable.
+print("Original value\'s :")
+print(age)
+print(height)
+print(name)
+print(is_student)
+'''or
+print(f"Original value: Age: {age}, Height: {height}, Name: {name}, Is Sudent: {is_student}")
+'''
+# 6. Increase the 'age' varable by 1 and print it out.
+age = age + 1
+"""print(age)"""
+'''or 
+age += 1
+print(age)
+'''
+# 7. Multiply the 'heght' variable by 100 to convert it to centimeters and print it out. 
+height_cm = height * 100
+"""print(f"{height_cm} cm")"""
+# 8. Add "is my name" to the 'name' variable and print it out.
+name +=' is my name'
+"""print(name)"""
+'''or
+name =name + is my name
+print(name)
+''' 
+# 9. Change 'Is_student' to the opposite and print it out.
+is_student = not is_student
+"""print(is_student)"""
+
+print(f"modified value\'s: Age: {age}, Height: {height}, Name: {name}, Is Sudent: {is_student}")
+
+'''
+print("Original value\'s :")
+print(age)
+print(height)
+print(name)
+print(is_student)
+'''
+print(f"Original value: Age: {age}, Height: {height}, Name: {name}, Is Sudent: {is_student}")
