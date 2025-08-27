0dc765f034179be771a1d907c437464307a2bdb7
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-27 18:47:07 +0000

    new and fresh codes which i baked rigth now and im happy that i have been prodoctive today ;)

---
+++ Variable/variable-simple-task.py
@@ -0,0 +1,8 @@
+# create integer addition and string output
+# string addition in the variable named new_msg(add a string to "my favourite food is ")
+new_msg01 = " ghorme sabzi its persian food"
+new_msg = "my favourite food is" + new_msg01
+print(new_msg)
+# integer addition in the variable named new_sum (add 2 or more integers)
+new_sum = 5 + 4 + 13
+print(new_sum) # create and print a new string variable, n new_msg_2, that contatenates new_msg + a literal string
