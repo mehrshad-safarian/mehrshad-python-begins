4fa041f8b8cb8e31ee13a9543458c18d253fbd8f
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-28 15:17:30 +0000

    new codes

---
+++ string/demo.py
@@ -0,0 +1,6 @@
+message = 'm0023s'
+
+print(len(message))
+
+print(message[4])
+print(message[0])
---
+++ string/input-string-formating.py
@@ -0,0 +1,4 @@
+fav_game = input('what is your favorite video game ?').upper()
+print(fav_game)
+fav_movie = input('what is your favorite movie ?')
+print(fav_movie.lower())
