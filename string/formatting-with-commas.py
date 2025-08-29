001a961fd03ca9b8e4bc659b7933322c0f474863
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-29 16:47:31 +0000

    final codesof today

---
+++ string/formatting-with-commas.py
@@ -0,0 +1,24 @@
+# Print three Strings on the same line using commas inside the print() function.
+name ="iran"
+print(name.upper(),"will be","great agian")
+# use a print() fuction with comma seperation to combine 2 numbers and 2 strings
+print("the",4,"th avenue beach","is in the seattle in the",4,"th Ave S" ) # i just wanted to write something that have 4th avenue in it after search google all f sudden gave me this its hilarious :)
+
+# print() comma separated mixing of strings and variables
+# and display text describing an address, made from strings and variables of 
+# different types
+age = 2500
+# The heart of Iranians civilization
+city = "Pasargadae"
+monument = "The tomb of Cyrus the Great"
+
+print(name.upper(),"is a",age,"year old country","The heart of Iranians civilization:", monument, "is located in", city)
+print("It is approximately", 135, "kilometers from Shiraz, taking around", 1.5, "hours to travel by car.")
+# print() number, strings, variables from input
+nationality = input("where are u from? ") # variable from input
+print("so u come from :",nationality, )
+country = nationality  
+if country in ["russia", "china", "france", "turkey", "palestine"]:
+    print("Ow ok!")
+else:
+    print("Nice to meet you!")
---
+++ string/training-class.py
@@ -0,0 +1,10 @@
+# create a program that prints out a reservation for a training class.
+# Gather the name of the party, the number attending and the time.
+
+# Gather reservation details
+party_name = input("name of the party ?")
+attendings_num = input("Enter the number of attending :") 
+time = input("Enter the time of the class :")
+
+# Print a full reservation message
+print("done !","party of :", party_name,"\t\nattender\'s number:",attendings_num,"\t\nclass time :",time)
