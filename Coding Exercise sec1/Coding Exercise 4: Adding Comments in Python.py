09a35a6682e71c6212db8c0efa0ed247861e0224
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-31 14:07:19 +0000

    new

---
+++ Coding Exercise sec1/Coding Exercise 4: Adding Comments in Python.py
@@ -0,0 +1,14 @@
+# Define a class named Person
+class Person:
+    # Define a method (function inside the class) to print person's info
+    def info(self):
+        # Printing the details
+        print("Name: Ali")
+        print("Age: 25")
+        print("Education: Bachelor's")
+
+# Create an object (instance) of the Person class
+p = Person()
+
+# Call the info() method to display the details
+p.info()
