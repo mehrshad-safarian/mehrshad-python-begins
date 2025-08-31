0310103a38ba26efcbfe91db4954ebcab6e48952
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-08-31 20:28:41 +0000

    last commit of today

---
+++ string/simple-calc.py
@@ -0,0 +1,22 @@
+# 1. Create two variables, num1 and num2, and assign them inter values. 
+num1 = 10
+num2 = 2
+
+# 2. Perform the followin
+# Addition of num1 and num2
+print(f"Additaion: {num1 + num2}")
+
+# Substraction of num1 and num2
+print(f"Substraction: {num1 - num2}")
+
+# Multiplication of num1 and num2
+print(f"Multiplication: {num1 * num2}")
+
+# Division of num1 and num2
+print(f"Division: {num1 / num2}")
+
+# Integer deviaion of num1 and num2
+print(f"Integer Devision: {num1 // num2}")
+
+# Remainder of num1 devided by num2
+print(f"Remainder: {num1 % num2}")
