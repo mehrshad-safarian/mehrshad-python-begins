fea2bcde9c6b6ce7ca8e4ebc1a40030ab6fbd335
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-09-01 15:20:04 +0000

    new

---
+++ string/strings_manipulation.py
@@ -0,0 +1,16 @@
+# Create a variable first_name and assignt to it your first name. 
+first_name = "mehrshad"
+# Create a variable last_name and assignt to it your last name. 
+last_name = "safarian"
+# Create a variable full_name by concatenating first_name and last_name with a space in between.
+full_name = "mehrshad_safarian"
+#  Print out the fuul_name.
+print(full_name)
+# Print out the length of full_name using the len()function.
+word_length = len(full_name)
+print(f"'{full_name}' has {word_length} characters")
+
+# Print out the full_name in all uppercase letters.
+print(full_name.upper())
+# Print out the full_name in all lowercase letters.
+print(full_name.lower())
