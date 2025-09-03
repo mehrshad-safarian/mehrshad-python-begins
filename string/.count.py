29600a4a4d7e433d5f1b4f5335af6de17acff617
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-09-03 19:35:02 +0000

    new code

---
+++ string/.count.py
@@ -0,0 +1,8 @@
+#count
+fullname = "mehrshad_safarian_khaki"
+print(f'letter "a" occurrence',{fullname.count("a")})
+print("how many i's? ", fullname.count("i"))
+print("how many m's? ", fullname.count("m"))
+print("uses '_' , how many time\'s? ", fullname.count("_"))
+print(f"first name is : ", fullname[:8])
+print(f"# n's in firstname:",{fullname[:8].count("n")})
