ed333962c9ff0f968098a0dd25b45e77e21f39ff
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-09-06 16:59:34 +0000

    new

---
+++ string/string-vs-list-mutability.py
@@ -0,0 +1,9 @@
+customer_name = 'behrshad'
+# customer_name[0] = 'j'
+modified_name = 'm' + customer_name[1:]
+print(modified_name)
+company_name = 'TechCorp'
+name_chars = list(company_name)
+name_chars[0] = 't'
+modified_company = ' '.join(name_chars)
+print(modified_company)
