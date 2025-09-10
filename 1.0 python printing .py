18fea015e6378fe0dea526d7a366b41cde099a3e
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-09-10 23:56:32 +0330

    Update 1.0 python printing .py

--- 1.0 python printing .py
+++ 1.0 python printing .py
@@ -3,7 +3,7 @@ print("hello, world!")
 print("python it's not that bad")
 print("im gladd for strarting to learn it")
 print("well acctualy it is fun")
-print("hope that make money from it soon !")
+print("hope that make money from it soon!")
 
 print('this is string with single quotes.!')
 print("this is string with double quotes.!")
@@ -12,6 +12,7 @@ print("my girl said,'honey python is awsome!' are you stil tryna to be one of th
 print('i replied,"not any more"')
 print("yea yea bla bla bal ,'thank for reading imagianary story that i created i wanted it sounds like batman story stuf XD'")
 print("honey if i ever find u in my life and u b t real 1 i will show this, u are my angle girl of course when u come into my life i forget all the girls ever been in my life ")
+
 
 print("this\tis\ttabbed")
 print("first line, 'metal gear solid 3 !'\nsecond line , 'GOD OF WAR 2(2007)' ")
