1fa440c9999165b1c0d5749b2594299175a5e5fc
mehrshad-safarian <safar911verydanger@gmail.com>
2025-09-24 13:21:29 +0330

    new

---
+++ my training/Define-a-Book-class.py
@@ -0,0 +1,21 @@
+class book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def display_info(self):
+        print("title:", self.title)
+        print("author:", self.author)
+        print("year:", self.year)
+        #def info(self):
+            # print(f"Title: {self.title}")
+            # print(f"Author: {self.author}")
+            # print(f"Year: {self.year}")
+
+title = input ("pls enter the title of ur book:")
+author = input("pls enter the name of author:")
+year = input("pls enter the year that the book released on :")
+
+ur_book = book(title, author, year)
+ur_book.display_info()
