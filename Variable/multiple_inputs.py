f96df1645f08cbba7a5b9b5c9c12517fd67d9f7b
mehrshad-safarian <safar911verydanger@gmail.com>
2025-09-28 23:28:19 +0330

    new

---
+++ Variable/multiple_inputs.py
@@ -0,0 +1,8 @@
+# create program that gathers the user's name, age, and hometown, then crafts
+# a summary sentence using this information.
+
+name = input("Pls Enter ur name :")
+age = input("How old ar u ?")
+hometown = input("where do u live ?")
+
+print(f"nice to know u {name.capitalize()} . so u are {age} years old !from lovely {hometown.capitalize()} ")
