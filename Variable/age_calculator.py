36f9be6834a687833f44f1b4dfe3b89bb527d88b
mehrshad-safarian <safar911verydanger@gmail.com>
2025-09-28 20:21:04 +0330

    new

--- Variable/age_calculator.py
+++ Variable/age_calculator.py
@@ -1,4 +1,6 @@
-current_year = 2025
+from datetime import datetime
+#current_year = 2025
+current_year = datetime.now().year
 birth_year = input("Pls enter the year u were born at :")
 age = current_year - int(birth_year)
 print(f"niiice".title(),"my friend! so u are", age ,"year old!")
