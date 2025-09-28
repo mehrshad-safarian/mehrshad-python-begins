32735f56c1c4916313ddcf45ee7b7b14851a926d
mehrshad-safarian <safar911verydanger@gmail.com>
2025-09-28 21:54:16 +0330

    new

---
+++ Variable/Echo_machine.py
@@ -0,0 +1,6 @@
+# Echo machine
+# Create simple program the will promt the user to type something and then
+# repeat it back exactly as entered
+
+Echo = input("Type anything u want and I'll whisper it back ;) :")
+print(Echo)
