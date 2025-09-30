c9cac9440e82a0b237fc339873a780bb8839bb87
mehrshad-safarian <safar911verydanger@gmail.com>
2025-09-30 23:59:37 +0330

    new code

---
+++ list/simple_append.py
@@ -0,0 +1,8 @@
+sample_list = [1, 2, 3]
+print("sample_list before:",sample_list)
+sample_list.append(7)
+print("\nsmaple_list after:",sample_list)
+sample_list.append("i never forget this day")
+print("\nwith new sentence:",sample_list)
+sample_list.append("8/7/02")
+print("\ndate:",sample_list)
