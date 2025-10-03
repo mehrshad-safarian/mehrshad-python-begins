a638fa23d4d0d0a4b282ce52c5c1a62610313a1a
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-03 23:59:02 +0330

    new

---
+++ list/party_list.py
@@ -0,0 +1,10 @@
+party_list = ["mehrshad","megan","sophie"]
+print(party_list)
+print("party list before:", party_luist, "\n")
+part_list[1] = "mehrshad"
+print("party list after:", party_list, "\n")
+party_list = ["mehrshad", "megan",:"sophie"]
+print(party_list)
+print("before:", party_list, "\n")
+party_list[1] = party_list[1] + "scarlet"
+print("after", party_list,"\n")
