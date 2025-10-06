dd773ff2316270e285aa5e102e80aaeba5d446ed
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-06 23:48:39 +0330

    new

---
+++ list/sort_and_filter.py
@@ -0,0 +1,12 @@
+foot_bones = ["calcaneus", "talus", "cuboid", "navicular", "lateral cuneiform",
+           "intermediate cuneiform", "medical cuneiform"]
+longer_names = ""
+shorter_names = ""
+
+for bone_name in foot_bones:
+    if len(bone_name) < 10:
+        shorter_names += bone_name + "\n"
+    else:
+        longer_names += bone_name + "\n"
+print("Short names:\n" + shorter_names)
+print("Long names:\n" + longer_names)
