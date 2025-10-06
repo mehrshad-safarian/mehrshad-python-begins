346ea375bc4bb9ae47fc1933780fe26361971a3e
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-06 23:30:01 +0330

    e

---
+++ list/for_in.py
@@ -0,0 +1,11 @@
+cities = ["new york", "tehran", "shiraz", "rasht", "lahijan", "aten", "sydney"]
+for city in cities:
+    print(city)
+print()
+
+sales = [6, 8, 11, 13, 23, 94, 15]
+total = 0 
+for sale in sales:
+    print(sale)
+    total +=sale
+print("total sales:", total)
