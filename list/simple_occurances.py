0af4755843139067cefd6e6c83a008beb2f0b09d
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-07 19:58:19 +0330

    new

---
+++ list/simple_occurances.py
@@ -0,0 +1,8 @@
+cities = ["New York", "Shanghai", "Munich", "Tokyo", "Dubai", "Mexico City", "São Paulo", "Hyderabad"]
+search_letter = "a"
+total = 0
+
+for city_name in cities:
+    total += city_name.lower().count(search_letter)
+
+print(f"The total # of \"{search_letter}\" found in the list is:", {total})
