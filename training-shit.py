d05a8e34da224c82369934b87225af42bdbbbf69
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-07 23:57:54 +0330

    new

--- training-shit.py
+++ training-shit.py
@@ -2,11 +2,16 @@
 # try:
 #     temp = float( input("Type a number to add: "))
 #     print( '100 = {} = {}'.format(temp, 100 + temp) )
-# # without try/except print statement woould not get hit if error occurs
+# # without try/except print statement would not get hit if error occurs
 # print("the program did not break!")
 
 # type a number to add: 11
 # 100 + 11.0 = 111.0
 # print("the program did not break !")
-a = [5, 10]
-print(id(a))
+for num in range (2, 10 ,2):
+    print("value: {}".format(num) )
+
+for num in range (5):
+    if num == 2:
+        continue
+    print(num)
