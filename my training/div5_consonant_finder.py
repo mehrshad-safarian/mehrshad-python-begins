01ce2698367a1fee9777bf360dc98beef0677d33
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-10-08 23:31:44 +0330

    Update div5_consonant_finder.py

--- my training/div5_consonant_finder.py
+++ my training/div5_consonant_finder.py
@@ -1,1 +1,12 @@
-#
+# to do : print numbers from 0 to 50 that are divisble by 5
+for i in range (0, 50):
+    if i % 5 == 0:
+        print(i)      
+        
+print()
+
+#to do : ask user to input a sentence and print only consonants(letters that are not vowels)
+sentence = input("pls enter ur sentence :")
+for letters in sentence :
+    if letters not in "aeiou" :
+        print(letters)
