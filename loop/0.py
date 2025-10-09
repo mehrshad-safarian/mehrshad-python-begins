686d9132dfe70ae33829d28d50deeb00e556a455
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-09 19:57:42 +0330

    new

---
+++ loop/0.py
@@ -0,0 +1,17 @@
+
+for num in range (2, 10 ,2):
+    print("value: {}".format(num) )
+print()
+
+
+for num in range (5):
+    if num == 2:
+        continue
+    print(num)
+print()
+
+
+for num in range (10):
+    if num == 4:
+        break 
+    print(num)
