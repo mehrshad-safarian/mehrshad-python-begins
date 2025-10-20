84bce9031587518e786e30583310b7bc7a53d3ae
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-21 00:59:18 +0330

    change

--- list/list_built_methods.py
+++ list/list_built_methods.py
@@ -1,5 +1,7 @@
 laptop_inventory = ["hp-2021-001", "dell-2022-002", "lenovo2023"]
 print(laptop_inventory)
+tablet_inventory = ["ipad--2025-001", "surface-2024-002"]
+print(tablet_inventory)
 laptop_inventory.append(input("pls enter ur considered laptop :"))
 print(laptop_inventory)
 
@@ -16,8 +18,27 @@ transferred_device = primery_inventory.pop()
 print(f"transferred device  : {transferred_device} ")
 
 acquired_inventory.clear()
-print(acquired_inventory)
+print(f"acquired_inventory: {acquired_inventory}")
 
 laptop_inventory.sort()
 laptop_inventory.reverse()
 print(laptop_inventory)
+
+laptop_inventory.insert(2,"asus-rog-2020")
+print(laptop_inventory)
+
+server_position = laptop_inventory.index("lenovo2023")
+print(f"server_position: {server_position}")
+dell_count = laptop_inventory.count("dell-2022-002")
+print(f"dell count: {dell_count}")
+
+
+complete_inventory = laptop_inventory + tablet_inventory
+print(f"complete_inventory: {complete_inventory}")
+
+branch_setups = tablet_inventory * 3
+print(f"branch_setups: {branch_setups}")
+
+total_tablet_inventory = len(tablet_inventory + branch_setups)
+print(f"total tablets: {total_tablet_inventory}")
+
