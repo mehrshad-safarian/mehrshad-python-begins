e5ef9481f8c27f701f0d1ddf5e163a0d00d2f45d
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-10-22 18:27:37 +0330

    Update guess_random_number.py

--- guess_random_number.py
+++ guess_random_number.py
@@ -1,6 +1,6 @@
 import random
 
-print("🎯 Welcome to the Number Guessing Game!")
+print(" Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100...")
 
 secret_number = random.randint(1, 100)
