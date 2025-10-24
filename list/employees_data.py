b70742c518347e84f3d7f49b43ccfb987db6bcdb
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-10-24 11:03:37 +0000

    new

--- list/employees_data.py
+++ list/employees_data.py
@@ -3,5 +3,9 @@ employees = [
     ["Hosea", "Right_hand", "80k"],
     ["Rat", "sponger", "30k"] #by rat i meant that bastard named Micah bell :)
 ]
-Right_hand_salaries = [emp[2] for emp in employees if emp[1] == "Right_hand"]
-print(Right_hand_salaries)
+# remove the "k" and convert to integer
+Right_hand_salaries = [int(emp[2].replace("k", "")) * 1000 for emp in employees if emp[1] == "Right_hand"]
+print(employees)
+print(f"Right handes fee : ${Right_hand_salaries[0]}, ${Right_hand_salaries[1]}")
+average_Right_hand_salaries = sum(Right_hand_salaries) / len(Right_hand_salaries)
+print(f" Average Right salary dutch payes : ${average_Right_hand_salaries}")
