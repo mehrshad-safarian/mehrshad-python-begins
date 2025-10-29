ec96bdc30201ce8ab5364dae58aeffeea42e2390
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-10-29 23:58:45 +0330

    Refactor department assignment printing logic

--- dictionaries/department.py
+++ dictionaries/department.py
@@ -1,1 +1,9 @@
+department_assignment = {'emp_101': 'sales', 'emp_102': 'finance', 'emp_103': 'IT'}
 
+# key_list = list(department_assignment.keys())
+# key_list.sort()
+# for employee_id in key_list:
+#     print(employee_id, '-->', department_assignment[employee_id])
+
+for  employee_id in sorted(department_assignment):
+    print(employee_id, '-->', department_assignment[employee_id])
