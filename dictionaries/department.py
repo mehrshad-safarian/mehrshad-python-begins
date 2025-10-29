c21d50a3c14c3e9e799eaaf501134c9abfc83285
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-10-30 00:09:53 +0330

    Add letter printing and shutdown countdown
    Added a loop to print letters of 'Iran' in uppercase and a countdown for system shutdown.

--- dictionaries/department.py
+++ dictionaries/department.py
@@ -7,3 +7,12 @@ department_assignment = {'emp_101': 'sales', 'emp_102': 'finance', 'emp_103': 'I
 
 for  employee_id in sorted(department_assignment):
     print(employee_id, '-->', department_assignment[employee_id])
+
+
+for letter in 'Iran':
+    print(letter.upper())
+
+time_remaining = 3
+while time_remaining > 0:
+    print('System shutdown in:' * time_remaining)
+    time_remaining  -=1
