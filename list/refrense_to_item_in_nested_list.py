cf89d2b487c2fc114b2a829994e6c93619c2c4ae
mehrshad-safarian <safar911verydanger@gmail.com>
2025-10-31 15:36:46 +0330

    change_maybe_idk!

--- list/refrense_to_item_in_nested_list.py
+++ list/refrense_to_item_in_nested_list.py
@@ -3,5 +3,5 @@ server_metrics = [["CPU_usage", "RAM_usage", "Disk_space"],
                 ["Response_time", "Error_rate", "Queue_length"]]
 resource_Metrics = server_metrics[0]
 print(resource_Metrics)
-network_in = server_metrics[1][0]
+network_in = server_metrics[1][0] #this is how we refer to spacial item we have in mind
 print(network_in)
