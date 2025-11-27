c7c447cd78423ab180419ef3101a735d76bba3e5
mehrshad-safarian <safar911verydanger@gmail.com>
2025-11-27 21:15:05 +0330

    new

--- if&else/dj(course)_0.py
+++ if&else/dj(course)_0.py
@@ -2,15 +2,12 @@ while True: # loop until valid input
     try:
         a = int(input("pls Enter number a (which should be int!): "))
         b = int(input("pls Enter number b (which should be int!): "))
-
         if b > a:
             print(f"{a} Is greater than {b}")
         elif b == a:
             print(f"{a} and {b} are equal!")
         else:
             print(f"{a} is smaller than {b}")
-
         break  #if anything is right get out of the loop
-
     except ValueError:
         print("Error: Please enter a valid integer number! (No decimals or letters allowed)\nTry again...\n")
---
+++ tuples/0.0try.py
@@ -0,0 +1,9 @@
+employee_record = ("Sophie Turner", "Actress", 29, "Northampton, England", "salary: 175k USD")
+data_components = (2025, 1, 10)
+
+project_phases = ("planning", "development", "testing", "deployment")
+print(len(project_phases))
+combined_phases = project_phases + ("maintenance",) 
+current_phase = project_phases[0]
+print(combined_phases)
+print(current_phase)
