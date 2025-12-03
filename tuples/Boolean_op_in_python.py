e589a0ed6be5b6f5f364b3103f586822028b58a8
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-03 23:54:27 +0330

    new

---
+++ tuples/Boolean_op_in_python.py
@@ -0,0 +1,6 @@
+# Boolean variables and values 
+is_raining = True 
+is_sunny = False
+
+weather = not is_raining  and is_sunny # True if it is not raining and it is sunny
+print(f"Is the weather nice? {weather}")
