2df27e52c6cc2bf166e6626e9b40d07e54754f4d
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-04 14:51:51 +0330

    changes

--- tuples/Boolean_op_in_python.py
+++ tuples/Boolean_op_in_python.py
@@ -2,5 +2,11 @@
 is_raining = True 
 is_sunny = False
 
-weather = not is_raining  and is_sunny # True if it is not raining and it is sunny
+weather = not is_raining and is_sunny  
 print(f"Is the weather nice? {weather}")
+
+
+'''bool(True) # Returns: True
+bool(False) # Returns: False '''
+print(bool(1))      # Returns: True
+print(bool(0))      # Returns: False
