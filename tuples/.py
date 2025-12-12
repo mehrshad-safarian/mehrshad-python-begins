d85ed54cbb651271a974a1f0e4d167bbae47c03b
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-12 13:54:03 +0330

    new

---
+++ tuples/.py
@@ -0,0 +1,13 @@
+def is_price_valid(price):
+    MIN_PRICE = 10.00   # minimum allowed price
+    MAX_PRICE = 99.99   # maximum allowed price
+
+    # returns True if price is within allowed range
+    return MIN_PRICE <= price <= MAX_PRICE
+print(f"50 is a valid price: {is_price_valid(50)}")   # True
+print(f"5 is a valid price: {is_price_valid(5)}")    # False
+input_price = int(input("Enter a price: "))
+if is_price_valid(input_price):
+    print("The price is valid.")
+else:
+    print("The price is invalid.")
---
+++ tuples/identity_operators.py
@@ -0,0 +1,12 @@
+# is&is not
+a = [1, 2, 3]
+b = [1, 2, 3]
+c = a 
+print(a is c)
+print(a is b)
+print(a is not b)
+print()
+
+# in&not in
+print(2 in a)
+print(6 not in a)
