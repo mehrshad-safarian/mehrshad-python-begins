670c600cc221450a9b8808755a0bc975c8336419
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-14 23:08:57 +0330

    change

--- clean_code_pep8_standard/Line_length.py
+++ clean_code_pep8_standard/Line_length.py
@@ -1,3 +1,12 @@
 # Pep 8 suggests keeping line of code to maximum 79 characters.
+
 # To long
 very_long_variable_name = some_function_with_a_very_long_name(long_argument1, long_argument2, long_argument3, long_argument4)
+
+# Better:
+very_long_variable_name = some_function_with_a_very_long_name(
+    long_argument1,
+    long_argument2,
+    long_argument3,
+    long_argument4
+)
---
+++ clean_code_pep8_standard/Naming_convintions.py
@@ -0,0 +1,11 @@
+""""1 def calculate_area(length, width): # Good
+2 def CalculateArea(length, width): # Not recommended
+3
+4 my_variable = 42 # Good
+5 myVariable = 42 # Not recommended
+6
+7 class CarEngine: # Good
+8 class car_engine: # Not recommended
+9
+10 MAX_SPEED = 120 # Good
+11 max_speed = 120 #""""
