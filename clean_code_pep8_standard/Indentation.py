210f728743efcbfeaf8c9925a90ac4af5a584c1e
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-14 22:02:56 +0330

    change

--- clean_code_pep8_standard/Indentation.py
+++ clean_code_pep8_standard/Indentation.py
@@ -1,5 +1,13 @@
 # Bad indentation 
+# def greet(name):
+# print(f"Hello, (name)!")
+#     print("Welcome to python")
+#         print("lets's learn about pep8!")
+
+# Good indentation
 def greet(name):
-print(f"Hello, (name)!")
+    print(f"Hello, {name}!")
     print("Welcome to python")
-        print("lets's learn about pep8!")
+    print("let's learn about pep8!")
+
+greet("Mehrshad")
