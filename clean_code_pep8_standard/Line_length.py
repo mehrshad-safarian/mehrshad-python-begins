0fa45e4d4870664af0637b175a9145a6ae9670c3
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-14 22:41:19 +0330

    new

---
+++ clean_code_pep8_standard/Line_length.py
@@ -0,0 +1,3 @@
+# Pep 8 suggests keeping line of code to maximum 79 characters.
+# To long
+very_long_variable_name = some_function_with_a_very_long_name(long_argument1, long_argument2, long_argument3, long_argument4)
