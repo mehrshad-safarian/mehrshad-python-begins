7aed045af0c5030c363d76348a083ac36cc92e61
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-17 23:53:38 +0330

    new

---
+++ clean_code_pep8_standard/pep_8_formatting_Ecercise/Example_3_Imports_and_Module_Level_Dunder_Names.py
@@ -0,0 +1,9 @@
+# Example 3: Imports and Module Level Dunder Names
+import os
+import sys
+
+import numpy as np
+from datetime import datetime,date,time
+
+__version__ = '1.0.0'
+__author__ = "John Doe"
