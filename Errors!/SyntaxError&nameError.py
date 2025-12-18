d28c29de7e788d2c2789d5b8f630d9eb6dbb5d18
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-18 21:38:27 +0330

    new

---
+++ Errors!/SyntaxError&nameError.py
@@ -0,0 +1,13 @@
+# 1) print needs parentheses → SyntaxError
+#    print("Hello") ✔️   |   print "Hello" ✘
+
+# 2) Strings must have matching quotes → SyntaxError
+#    "Hi" ✔️   |   "Hi ✘
+
+# 3) print must be lowercase and spelled right → NameError
+#    print("Hi") ✔️   |   Print("Hi") ✘
+
+# 4) Variable names cannot start with a number → SyntaxError
+#    my_var = 5 ✔️   |   1st_var = 5 ✘
+
+type(3.3( 
