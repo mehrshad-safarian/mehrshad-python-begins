39b7511df7e8ac4eaa32cf31c0e37f1b2e39e4d4
mehrshad (mohamad) safarian <63556723+mehrshad-safarian@users.noreply.github.com>
2025-12-18 09:35:11 +0000

    change

--- clean_code_pep8_standard/pep_8_formatting_Ecercise/Example_5.py
+++ clean_code_pep8_standard/pep_8_formatting_Ecercise/Example_5.py
@@ -11,7 +11,27 @@ Insufficient docstring
 Multiple statements on one line
 """
 
+# Example 5: Comments and Docstrings
+#this function calculates the factorial of a number
 def factorial(n):
-    """Calculate factorial"""
-    if n == 0: return 1
-    else: return n * factorial(n - 1)
+    """
+    Calculate factorial of a number.
+
+    Args:
+        n (int): The number to calculate the factorial for.
+
+    Returns:
+        int: The factorial of n.
+    """
+    if n == 0:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+# Get input from the user
+number = int(input("Please enter a number: "))
+
+# Call the function and print the result
+result = factorial(number)
+print(result)
