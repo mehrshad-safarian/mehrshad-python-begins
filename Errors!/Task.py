81b31939de830a0347a9fadf5888a3c16668c280
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-18 21:47:24 +0330

    new

---
+++ Errors!/Task.py
@@ -0,0 +1,32 @@
+# repair the syntax error
+# print('my socks do not match")
+# repair the NameError
+# pront("my socks match now")
+# repair the syntax error
+# print"Save the notebook frequently")
+# repair the NameError
+# student_name = "Alton"
+# print(STUDENT_NAME)
+# repair the TypeError
+# total = 3
+#print(total + " students are signed up for tutoring")
+
+
+
+
+# repair the syntax error
+print("my socks do not match")
+
+# repair the NameError
+print("my socks match now")
+
+# repair the syntax error
+print("Save the notebook frequently")
+
+# repair the NameError
+student_name = "Alton"
+print(student_name)
+
+# repair the TypeError
+total = "3"
+print(total + " students are signed up for tutoring")
