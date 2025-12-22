73423b1383a1d177ad37d6906b3b049ffa1fff8b
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-23 00:49:36 +0330

    new

---
+++ Functions/Built_in_funs.py
@@ -0,0 +1,10 @@
+# Python has 69 Built-in Functions (as of version 3.8)
+# Type conversion : str(), int(), float(), bool(), list(), tuple(), set(), dict()
+# numeric operations : round(), sum() min(), max()
+# Object information : type(), len()
+
+len("111") # Returns 3
+int("111") # Returns 111
+float("111") # Returns 111.0
+sum((3,5.4)) # Returns 5.8
+print(sum)
