a7ff7521f1ca6ea18fbcf0638aef28eac2b9a96d
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-22 23:43:11 +0330

    new

---
+++ Functions/print_func.py
@@ -0,0 +1,6 @@
+# Can be called with no arguments 
+# Can be called with one or more arguments
+# Outputs to the console
+print("Hello, World!")  # Single argument
+print()                 # No arguments
+print("Bye", "then") # Multiple arguments
