6f20b0df5bff7bc4fe57d8316dd3f6d92a240ffe
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-25 23:02:07 +0330

    new

---
+++ Functions/Functions.py
@@ -0,0 +1,21 @@
+# The print () function
+print("Hellooo!")
+print()
+print("bye byyeee!!!")
+
+# Using len
+number00 = "789"
+print(len(number00))
+
+my_list = [7, 8, 4, 3, 2, 9]
+print(len(my_list))
+
+# Type conversion
+number01 = "123"
+integer = int(number01)
+float_number = float(number01)
+
+# mathematician operations
+integer = 7
+float_number = 13.1333
+print(sum((integer,float_number)))
