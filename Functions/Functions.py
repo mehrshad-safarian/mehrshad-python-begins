e40e512642aa47425128da16222d54f4fb25ea93
mehrshad-safarian <safar911verydanger@gmail.com>
2025-12-26 21:38:16 +0330

    changes

--- Functions/Functions.py
+++ Functions/Functions.py
@@ -1,70 +1,86 @@
-# The print () function
-print("Hellooo!")
-print()
-print("bye byyeee!!!")
+# # The print () function
+# print("Hellooo!")
+# print()
+# print("bye byyeee!!!")
 
-# Using len
-number00 = "789"
-print(len(number00))
+# # Using len
+# number00 = "789"
+# print(len(number00))
 
-my_list = [7, 8, 4, 3, 2, 9]
-print(len(my_list))
+# my_list = [7, 8, 4, 3, 2, 9]
+# print(len(my_list))
 
-# Type conversion
-number01 = "123"
-integer = int(number01)
-float_number = float(number01)
+# # Type conversion
+# number01 = "123"
+# integer = int(number01)
+# float_number = float(number01)
 
-# mathematician operations
-integer = 7
-float_number = 13.1333
-print(sum((integer, float_number)))
+# # mathematician operations
+# integer = 7
+# float_number = 13.1333
+# print(sum((integer, float_number)))
 
-my_sum = sum((integer, float_number))
-print(round(my_sum))
+# my_sum = sum((integer, float_number))
+# print(round(my_sum))
 
-print(min(integer, float_number))
-print(max(integer, float_number))
+# print(min(integer, float_number))
+# print(max(integer, float_number))
 
-# Combining Functions 
-numbers = []  # list to store the numbers
+# # Combining Functions 
+# numbers = []  # list to store the numbers
 
-while True:
-    user_input = input("Enter a number (type 'exit' to stop): ")
+# while True:
+#     user_input = input("Enter a number (type 'exit' to stop): ")
     
-    if user_input.lower() == "exit":
-        print("Exiting...")
-        break
+#     if user_input.lower() == "exit":
+#         print("Exiting...")
+#         break
 
-    try:
-        number = float(user_input)
-        numbers.append(number)  # add the number to the list
-        print("You entered:", number)
-    except ValueError:
-        print("Invalid input! Please enter a number or type 'exit'.")
+#     try:
+#         number = float(user_input)
+#         numbers.append(number)  # add the number to the list
+#         print("You entered:", number)
+#     except ValueError:
+#         print("Invalid input! Please enter a number or type 'exit'.")
 
-print("\nAll numbers you entered:")
-print(numbers)  # this is a list
-"""How the numbers were stored in a list
-1. Create an empty list:
-numbers = []
-2. Every time the user enters a valid number, add it to the list using:
-numbers.append(number)
-Key points to remember
-[] = list
-() = tuple
-.append() works only on lists (because lists are mutable)
-Every append places the new value at the end of the list
-In one sentence
-We used numbers = [] to create a list, and each time a number was entered, numbers.append(number) added it to the list.
-"""
-average = sum(numbers) / len(numbers)
-print(f"average of ur number's is :{average}")
-rounded_avg_num = round(average)
-print(f"Rounded avg og ur number is: {rounded_avg_num}")
+# print("\nAll numbers you entered:")
+# print(numbers)  # this is a list
+# """How the numbers were stored in a list
+# 1. Create an empty list:
+# numbers = []
+# 2. Every time the user enters a valid number, add it to the list using:
+# numbers.append(number)
+# Key points to remember
+# [] = list
+# () = tuple
+# .append() works only on lists (because lists are mutable)
+# Every append places the new value at the end of the list
+# In one sentence
+# We used numbers = [] to create a list, and each time a number was entered, numbers.append(number) added it to the list.
+# """
+# average = sum(numbers) / len(numbers)
+# print(f"average of ur number's is :{average}")
+# rounded_avg_num = round(average)
+# print(f"Rounded avg og ur number is: {rounded_avg_num}")
 
-# Using type()
-mixed_list = [1, "three", 3.745, [9, 10]]
-for item in mixed_list:
-    print(f"The type of {item}, is{type(item)}")
-    
+# # Using type()
+# mixed_list = [1, "three", 3.745, [9, 10]]
+# for item in mixed_list:
+#     print(f"The type of {item}, is{type(item)}")
+
+
+# String Manipuation
+message = "hello, world!"
+print(message.upper())
+
+words = message.split(", ")
+print(words)
+
+print(", ".join(word.capitalize() for word in words))
+
+fruits = ['apple', 'pineapple', 'orange']
+print(", ".join(fruits))
+print(" - ".join(fruits))
+
+word = "JavaScript "
+print("".join(reversed(word)))
