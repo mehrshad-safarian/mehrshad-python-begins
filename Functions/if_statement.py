1fb721a624e80f1aa7101e876cb186914f3ecd12
mehrshad-safarian <safar911verydanger@gmail.com>
2026-01-02 16:44:28 +0330

    new

---
+++ Functions/if_statement.py
@@ -0,0 +1,9 @@
+# Using an if statement to only run code if the condition is met 
+x, y = 5, 10 
+if x < y :
+    print("x is less than y")
+
+# Checking user inputs 
+tmp = int(input("What is 5 + 5?"))
+if tmp == 10:
+    print(" U got it right !")
